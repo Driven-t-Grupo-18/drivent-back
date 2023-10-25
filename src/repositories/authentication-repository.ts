d78e5016dfb49781a80bf692e24d0836d551e434
Brendo <brendow_prime@hotmail.com>
2023-10-24 21:36:05 -0400

    correção ponto e virgula

--- src/repositories/authentication-repository.ts
+++ src/repositories/authentication-repository.ts
@@ -6,25 +6,23 @@ async function createSession(data: Prisma.SessionUncheckedCreateInput) {
   const result = await prisma.session.create({
     data,
   });
-  await setRedis(`user-${result.token}`, JSON.stringify(result))
-  return result
+  await setRedis(`user-${result.token}`, JSON.stringify(result));
+  return result;
 }
 
 async function findSession(token: string) {
-
-  const redis = JSON.parse(await getRedis(`user-${token}`))
-  if(redis){
-    return redis
-  } else{
-
+  const redis = JSON.parse(await getRedis(`user-${token}`));
+  if (redis) {
+    return redis;
+  } else {
     const result = await prisma.session.findFirst({
       where: {
         token,
       },
     });
 
-    await setRedis(`user-${token}`, JSON.stringify(result))
-    return result
+    await setRedis(`user-${token}`, JSON.stringify(result));
+    return result;
   }
 }
 
