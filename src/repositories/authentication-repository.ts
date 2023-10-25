a008ebd3a98456dfd1e6b695a27467016c37d631
Lucas Casal <lucasocteixeira@gmail.com>
2023-10-24 22:31:53 -0300

    redis: ajustes 2

--- src/repositories/authentication-repository.ts
+++ src/repositories/authentication-repository.ts
@@ -1,18 +1,31 @@
 import { Prisma } from '@prisma/client';
 import { prisma } from '@/config';
+import { getRedis, setRedis } from '@/redisConfig';
 
 async function createSession(data: Prisma.SessionUncheckedCreateInput) {
-  return prisma.session.create({
+  const result = await prisma.session.create({
     data,
   });
+  await setRedis(`user-${result.token}`, JSON.stringify(result))
+  return result
 }
 
 async function findSession(token: string) {
-  return prisma.session.findFirst({
-    where: {
-      token,
-    },
-  });
+
+  const redis = JSON.parse(await getRedis(`user-${token}`))
+  if(redis){
+    return redis
+  } else{
+
+    const result = await prisma.session.findFirst({
+      where: {
+        token,
+      },
+    });
+
+    await setRedis(`user-${token}`, JSON.stringify(result))
+    return result
+  }
 }
 
 export const authenticationRepository = {
